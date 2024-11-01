8f1271b54449d99befce1ca2f85cdb3a5bc7f712
project-first-html <alwls1739@gmail.com>
2024-11-01 10:59:08 +0900

    member entity 입력중

--- src/main/java/com/volunteer/Entity/Member.java
+++ src/main/java/com/volunteer/Entity/Member.java
@@ -1,7 +1,24 @@
 package com.volunteer.Entity;
 
-import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
+@Table(name="Member")
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "member_id")
+    private int id;
+
+    private String memberUserId;
+    private String memberNickname;
+    private String memberPassword;
+    private String memberEmail;
+    private String memberAddress;
+
 }
