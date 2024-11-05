7afc10f7673fab89667854fa7b3a3cfe9766c178
jung6A <heide6@naver.com>
2024-11-05 15:44:38 +0900

    마이페이지 컨트롤

---
+++ src/main/java/com/volunteer/Control/MypageControl.java
@@ -0,0 +1,20 @@
+package com.volunteer.Control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mypage")
+public class MypageControl {
+    //마이페이지 메인
+    @GetMapping("")
+    public String mypage() {
+        return "mypage/mypageIndex";
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        return "mypage/myinfo";
+    }
+}
