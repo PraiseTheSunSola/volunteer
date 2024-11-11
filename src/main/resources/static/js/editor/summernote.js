669b9eb689b14a6d0294c4c1f55a24577ffc0fb6
jung6A <heide6@naver.com>
2024-11-11 17:34:29 +0900

    웹에디터, 신고 팝업 css 정리

--- src/main/resources/static/js/editor/summernote.js
+++ src/main/resources/static/js/editor/summernote.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
     $('#summernote').summernote({
-        lang: 'ko-KR'
+        height: 400,
+        lang: 'ko-KR',
+        toolbar: [
+            ['fontname', ['fontname']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['style', ['bold', 'italic', 'underline','strikethrough', 'clear']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']]
+        ],
+        fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New','맑은 고딕','궁서','굴림체','굴림','돋움체','바탕체'],
+        fontSizes: ['8','9','10','11','12','14','16','18','20','22','24','28','30','36','50','72']
     });
 });
