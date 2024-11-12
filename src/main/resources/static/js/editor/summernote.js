e6d07c9cdcffe655bde14ef2deebf5005e4044a0
jung6A <heide6@naver.com>
2024-11-12 14:44:59 +0900

    관리자 페이지 css

--- src/main/resources/static/js/editor/summernote.js
+++ src/main/resources/static/js/editor/summernote.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#summernote').summernote({
-        height: 400,
+        height: 550,
         lang: 'ko-KR',
         toolbar: [
             ['fontname', ['fontname']],
@@ -8,9 +8,15 @@ $(document).ready(function() {
             ['color', ['color']],
             ['style', ['bold', 'italic', 'underline','strikethrough', 'clear']],
             ['para', ['ul', 'ol', 'paragraph']],
-            ['height', ['height']]
+            ['height', ['height']],
+            ['insert',['picture','link','video']],
+            ['view', ['fullscreen', 'help']],
+            ['table', ['table']]
         ],
-        fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New','맑은 고딕','궁서','굴림체','굴림','돋움체','바탕체'],
-        fontSizes: ['8','9','10','11','12','14','16','18','20','22','24','28','30','36','50','72']
+        fontNames: ['맑은 고딕', '궁서', '굴림체', '굴림', '돋움체', '바탕체', 'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New'],
+        fontSizes: ['8','9','10','11','12','14','16','18','20','22','24','28','30','36','50','72'],
     });
+    $('#summernote').summernote('fontName', '맑은 고딕');
+    $('#summernote').summernote('fontSize', 14);
+    $('#summernote').summernote('fontSizeUnit', 'pt');
 });
