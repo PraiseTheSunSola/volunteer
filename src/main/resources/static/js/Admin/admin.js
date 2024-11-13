d84bd135ea56056661f8cc1484fc6cf4faf6f0eb
jung6A <heide6@naver.com>
2024-11-13 15:39:31 +0900

    주석 추가

--- src/main/resources/static/js/Admin/admin.js
+++ src/main/resources/static/js/Admin/admin.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //페이지를 불러왔을 때 카테고리 선택에 따라 화면 변경
     var category = $("#categorySelect option:selected").val();
 
     if(category=='volunteer'){
@@ -10,6 +11,7 @@ $(document).ready(function(){
     }
 
     $("select[name=categorySelect]").change(function(){
+        //카테고리 선택을 수정할 때마다 화면 변경
         category = $("#categorySelect option:selected").val();
 
         if(category=='volunteer'){
