9908c837f34b9ba18b9239c3fcd926d9d0d6c186
falterguy <yeong4573@nate,com>
2024-11-15 20:36:05 +0900

    11.15소개페이지 코드개선

--- src/main/java/com/volunteer/Control/IntroControl.java
+++ src/main/java/com/volunteer/Control/IntroControl.java
@@ -1,20 +1,26 @@
 package com.volunteer.Control;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping( "/intro" )
 public class IntroControl {
 
     // 소개 메인페이지
-    @GetMapping("information")
-    public String intro() {return "intro/information";}
+    @GetMapping("/information")
+    public String intro() {
+        return "intro/information";  // 반환 경로: /src/main/resources/templates/intro/information.html
+    }
 
     // 미션 소개페이지
     @GetMapping("/mission")
-    public String mission() {return "intro/mission";}
+    public String mission() {
+        return "intro/mission";  // 반환 경로: /src/main/resources/templates/intro/mission.html
+    }
 
 }
