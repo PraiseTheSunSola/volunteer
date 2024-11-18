f7514dade7d98692c2531b4ed414288c34aca29d
lmjin044 <b_pen@naver.com>
2024-11-18 19:11:52 +0900

    검색 결과 구현, 회원가입, 로그인, ID-PW 찾기 페이지 구성중

--- src/main/java/com/volunteer/Config/SecurityConfig.java
+++ src/main/java/com/volunteer/Config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.volunteer.Config;
 
+import com.volunteer.Service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +15,8 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    MemberService memberService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
