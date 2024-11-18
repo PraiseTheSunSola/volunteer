01a87a6d677c04696f395227f995c1495fb747f9
jung6A <heide6@naver.com>
2024-11-18 18:09:46 +0900

    글을 저장하게 하기 위한 분투 (실패)

--- src/main/java/com/volunteer/Service/AdminService.java
+++ src/main/java/com/volunteer/Service/AdminService.java
@@ -20,21 +20,6 @@ public class AdminService {
 
     public void contentSave(ContentDto contentDto){
         ContentEntity contentEntity = contentDto.createEntity();
-        System.out.println(contentEntity.getContent());
-        System.out.println(contentEntity.getTitle());
-        System.out.println(contentEntity.getAge());
-        System.out.println(contentEntity.getId());
-        System.out.println(contentEntity.getCategory());
-        System.out.println(contentEntity.getAgencyName());
-        System.out.println(contentEntity.getAgencyTel());
-        System.out.println(contentEntity.getAgencyEmail());
-        System.out.println(contentEntity.getGroup());
-        System.out.println(contentEntity.getRecruitCount());
-        System.out.println(contentEntity.getGoalCost());
-        System.out.println(contentEntity.getRecruitStart().toString());
-        System.out.println(contentEntity.getRecruitEnd().toString());
-        System.out.println(contentEntity.getActivityStart().toString());
-        System.out.println(contentEntity.getActivityEnd().toString());
 
         contentRepository.save(contentEntity); //테이블에 저장
     }
